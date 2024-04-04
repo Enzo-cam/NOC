757e627dfd50640ea6ae634e3223f4b7d3acf84d
Enzo-cam <camerac09@gmail.com>
2024-04-04 15:45:00 -0300

    Feat: implementation of FileSystem with Datasource

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,5 +1,11 @@
 import { CronService } from "./cron/cron-service";
 import { CheckService } from "../domain/use-cases/checks/check-service";
+import { LogRepositoryImpl } from "../infrastructure/Repositories/LogRepository-impl";
+import { FileSystemDS } from "../infrastructure/Datasources/FileSystem.ds";
+
+const FileSystemLogRep = new LogRepositoryImpl(
+  new FileSystemDS()
+)
 
 export class Server {
   static start() {
@@ -7,6 +13,7 @@ export class Server {
 
     CronService.createJob("*/5 * * * * *", () => {
       new CheckService(
+        FileSystemLogRep,
         () => console.log("All good"),
         (error) => console.log(`${error}`)
       ).execute("http://localhost:3000/posts");
