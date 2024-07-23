8bc316e1aa7dfad06a9d552f3d0159a37dc8e732
Enzo-cam <camerac09@gmail.com>
2024-07-23 15:47:27 -0300

    Tested email UC and Serv

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -2,21 +2,35 @@ import { CronService } from "./cron/cron-service";
 import { CheckService } from "../domain/use-cases/checks/check-service";
 import { LogRepositoryImpl } from "../infrastructure/Repositories/LogRepository-impl";
 import { FileSystemDS } from "../infrastructure/Datasources/FileSystem.ds";
+import { EmailService } from "./email/email-service";
+import { SendEmailLogs } from "../domain/use-cases/emails/email-logs";
 
-const FileSystemLogRep = new LogRepositoryImpl(
-  new FileSystemDS()
-)
+const FileSystemLogRep = new LogRepositoryImpl(new FileSystemDS());
+const emailService = new EmailService();
 
 export class Server {
   static start() {
-    console.log("Servidor arrancando.");
+    // console.log("Servidor arrancando.");
 
-    CronService.createJob("*/5 * * * * *", () => {
-      new CheckService(
-        FileSystemLogRep,
-        () => console.log("All good"),
-        (error) => console.log(`${error}`)
-      ).execute("http://localhost:3000/posts");
+    // new SendEmailLogs(emailService, FileSystemLogRep).executeAttachments([
+    //   "enzo.fcam@gmail.com",
+    //   "camerac09@gmail.com",
+    // ]);
+    new SendEmailLogs(emailService, FileSystemLogRep).executeText({
+      to: "enzo.fcam@gmail.com",
+      subject: "Testing NOC",
+      htmlBody: "Testeando el NOC a ver si anda o no",
     });
+
+    // emailService.sendEmailWithAttachment(
+    // );
+
+    // CronService.createJob("*/5 * * * * *", () => {
+    //   new CheckService(
+    //     FileSystemLogRep,
+    //     () => console.log("All good"),
+    //     (error) => console.log(`${error}`)
+    //   ).execute("http://localhost:3000/posts");
+    // });
   }
 }
